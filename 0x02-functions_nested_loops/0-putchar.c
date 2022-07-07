26cc9c76c7479ee28417be0c31203a61fa7b1fa3
SAMKEZ2022 <kodjom25@gmail.com>
2022-07-06 18:05:17 -0700

    winner

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
  *
@@ -40,24 +39,3 @@ int main(void)
 }
 
 
-=======
-/**
- * main - prints _putchar
- * @void: doesn't take any parameter
- *
- * Return: 0 (always success)
- */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
->>>>>>> 99622ccbc0f2756519b3847df59ec2b2e514e7f1
