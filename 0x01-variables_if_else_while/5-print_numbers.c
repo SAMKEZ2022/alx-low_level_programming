61d546c95e035b96098295cbd9e2ff676905a7ab
SAMKEZ2022 <107487591+SAMKEZ2022@users.noreply.github.com>
2022-07-15 12:45:23 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,18 @@
-stg
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void - no parameter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
