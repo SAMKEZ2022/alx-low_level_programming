12a74c380bb535d9496d69b1642088743b7b3105
SAMKEZ2022 <107487591+SAMKEZ2022@users.noreply.github.com>
2022-08-03 01:25:05 +0000

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - calles the name_caller function
- *
- *   * Return: 0 (always success)
- *
- *    */
-
+ * main - calles the name_caller function
+ * Return: 0 (always success)
+ */
 int main(void)
-
 {
-
-		printf("%s\n", __FILE__);
-
-			return (0);
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
-
-
